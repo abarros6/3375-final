5e0621cedab5f85d5dff6bfac3994af73e3490e0
Anthony <anthonyb@versa-networks.com>
2024-04-01 15:32:33 -0400

    fix: changed folder structure and ported lab 3 into testing.c

--- project/testing.c
+++ project/testing.c
@@ -1,10 +1,10 @@
 #include <stdint.h>
 
-#define SW_BASE               0xFF200040
+#define SW_BASE               0xFF200040//switch hardware
 #define ADC_BASE              0xFF204000//Analog-to-Digital Convertor
 #define JP1_BASE              0xFF200060//GPIO port
-#define KEY_BASE              0xFF200050
-#define HEX3_HEX0_BASE        0xFF200020
+#define KEY_BASE              0xFF200050//button hardware
+#define HEX3_HEX0_BASE        0xFF200020//hex output hardware location
 
 volatile int* btnHardware = (int*) KEY_BASE;
 volatile int* hex_ptr = (int*) HEX3_HEX0_BASE;
@@ -38,12 +38,50 @@ int ReadSwitch(void)// read the lowest switch and interpret the instructions
     return switch_val;//return converted value
 }
 
+
+// BELOW FUNCTION IS CURRENTLY UNUSED, BROUGHT OVER FROM LAB 3
+// WE WILL LIKELY BE CONVERTING THIS FUNCTION TO TRANSLATE THE POTENTIOMETER 
+//OUTPUT TO THE PWM INPUT
+//Function to write to the GPIO pins:
+void LightPins(int num){
+    num = num*11/4096;//10 pins handling 12 bits (2^12==4096)
+    //write binary string containing num+1 1s in a row
+    int pinWrites[11] = {
+        0x0, 0x1, 0x3, 0x7,
+        0xF, 0x1F, 0x3F, 0x7F,
+        0xFF, 0x1FF, 0x3FF
+    };
+    
+    //Map the value accordingly
+    if(num<11) jp1Controller->data = pinWrites[num];
+    else jp1Controller->data &= 0;//clear out if number is too big to avoid damaging the LEDs
+}
+
 int main() {
+
+    // LAB 3 code START //
+
+    jp1Controller->directCntrl |= 0x3FF;//set pins 0 through 9 as output
+    volatile int delay_count;//to track delaying for loop
+    //polling much faster than internal timers, so using main board to calculate delay
+    int DELAY_LENGTH = 100;//to prevent bouncing
+    volatile int val;//used to read from specific ADC channels
+
+    // LAB 3 code END  //
+
     int current_value = 0; // Initial value of the hex number
     int previous_btn_state = 0; // Keep track of the previous state of the buttons
     int switchValue;
 
     while (1) {
+
+        /* CURRENTLY UNUSED LAB 3 CODE FOR INTERACTING WITH ADC
+
+            adcController[1] = 0x1;
+            val = adcController[1] & 0xFFF;//only preserve bits 0 through 11
+            LightPins(val);
+        
+        */
 
         if(ReadSwitch()) {
             //if swtich 0 is on then switch direction to reverse
