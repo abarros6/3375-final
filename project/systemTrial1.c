4560ddead7ffae225cc17e9f69b5b4aa8ea5ac86
Virain <vbawa@uwo.ca>
2024-04-03 16:00:03 -0400

    updated comments

--- project/systemTrial1.c
+++ project/systemTrial1.c
@@ -1,10 +1,12 @@
-// Motor Driver 
+// Motor Driver - ECE 3375B
+// Anthony Barros, Bashshar Atif, Muhammad Shahzad, Virain Bawa
+// March 24, 2024
 #include <stdint.h>
 #include <stdio.h>
 
 // define gpio pins for motor controller (probably will have to change accordingly)
-#define PHASE_PIN 	31  	// define actual pin number for Phase2 (D35)
-#define ENABLE_PIN 	30 		// define actual pin number for Enable2 (PWM) (D34)
+#define PHASE_PIN 	31  	// actual pin number for Phase2 (D31)
+#define ENABLE_PIN 	30 		// actual pin number for Enable2 (PWM) (D30)
 
 // define for GPIO and private timer check addresses and ADC addresses and direction switch 
 #define PRIV_TIME 		0xFFFEC600		// private timer
@@ -15,17 +17,19 @@
 #define HEX3_HEX0_BASE        	0xFF200020		//hex outpdware location
 
 // System configurations 
-#define LOAD 200000		// 100 MHz (change acordingly)		
+#define LOAD 200000		// (change acordingly)		
 
 //ADC struct (8 sets of 32-bit words):
 volatile unsigned int *adcController = (unsigned int *)ADC_BASE;	// Holds adc value 
 volatile int pinState = 0;						// for deciding when the timer is high value or low value 
 
 //button, switch, and hex display
-volatile int* btnHardware = (int*) KEY_BASE;
+volatile int* btnHardware = (int*) KEY_BASE;	
 volatile int* hex_ptr = (int*) HEX3_HEX0_BASE;
-volatile int *switch_ptr = (int *)SW_BASE;				// reads the switches 
-volatile int switch_val;						// to store switch value 
+volatile int *switch_ptr = (int *)SW_BASE;			// reads the switches 
+volatile int switch_val;							// to store switch value 
+
+// digits fore hex display 
 volatile int hex_code[16] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};	
 		
 volatile int current_value = 0; 	// Initial value of the hex number
@@ -36,10 +40,11 @@ typedef struct
 {
     uint32_t data;						//Data register
     uint32_t directCntrl;				//Direction Control register
-    uint32_t interruptEnCntrl;			//Interrupt Enable Control register
+    uint32_t interruptEnCntrl;			//Interrupt Enable Control register 
     uint32_t edgeCptrStatus;			//Edge-Capture Status register
 } JP1_Struct;
 
+// instantiate the jp1 controller
 JP1_Struct *jp1Controller = (JP1_Struct *)JP1_BASE; // for the leds 
 
 // Timer struct 
@@ -47,7 +52,7 @@ typedef struct {
 	uint32_t load;			//load value (one period)
 	uint32_t counter;		//current value 
 	uint32_t control; 		//control setting (interrupt -maybe, auto restart - maybe, Enable)
-	uint32_t status; 		// interrupt status 
+	uint32_t status; 		//interrupt status 
 } PrivateTimer;
 
 // instantiate the timer 
@@ -55,12 +60,11 @@ volatile PrivateTimer *timer = (PrivateTimer *)PRIV_TIME;
 
 // Initialize pin used for GPIO output PWM 
 void GPIO_Init(){
-	jp1Controller->directCntrl |= 0xFFFFFFFF;//set pins 0 through 9 as output and pins 30 and 31 as input
+	jp1Controller->directCntrl |= 0xFFFFFFFF;//set pins to output 
 }
 
-// set the pin to higher low 
+// set a specific pin to higher low 
 void GPIO_Set(int pin, int value){
-	// write the code here depending on if The motor controller is with jp1 or if its a seperate GPIO
    	if (value) {
        	jp1Controller->data |= (1 << pin);  // Set pin high
 		printf("high: %d\n", pin);
@@ -74,15 +78,17 @@ void GPIO_Set(int pin, int value){
 void InitTimer(uint32_t loadValue){
 	timer->load = loadValue;	// set the load value (one period duration)
 	// maybe dont auto start and explicity restart it in sigControl every time the flag is set 
-	timer->control = 0b11;		// enables
+	timer->control = 0b11;		// enables and auto restarts 
 	timer->status = 1; 			// Just to make sure and clear the flag
 }
 
+// reading the poteniometer value
 int ReadADC(void) {
 	adcController[0] = 0x1; 			// start adc conversation for channel 0 
 	return adcController[0] & 0xFFF;	// return the 12 bit adc result 
 }
 
+// displaying the hex digit
 void displayHexDigit(int digit) {
     *hex_ptr = hex_code[digit];
 }
@@ -90,8 +96,8 @@ void displayHexDigit(int digit) {
 //Function to retreive SW0's value
 int ReadSwitch(void)// read the lowest switch and interpret the instructions
 {
-    switch_val = *(switch_ptr) &= 0x01;//convert retrieved value from the lowest switch
-    return switch_val;//return converted value
+    switch_val = *(switch_ptr) &= 0x01;	//convert retrieved value from the lowest switch
+    return switch_val;					//return converted value
 }
 
 //Function to write to the GPIO pins:
@@ -137,41 +143,38 @@ void ButtonControl()
 
 // polling the timer 
 void SigControl(){
-	// read potentiometer and light lens
+	// read potentiometer and light the leds 
 	int adcValue = ReadADC();
 	LightPins(adcValue);
+
+	// for printing out values to trouble shoot 
 	int flag = timer->status; //
 	int loadval = timer->counter;
 	printf("ADC Value: %d, Flag: %d, Load Value: %d\n", adcValue, flag, loadval);
+
 	// Read switch for direction control and set phase pin
-	// see if our bird supports this method otherwise use two phase pins to control it or use a H-bridge 
-       	int direction = ReadSwitch();
-        GPIO_Set(PHASE_PIN, direction);
+    int direction = ReadSwitch();
+    GPIO_Set(PHASE_PIN, direction);
+
 	if (timer->status & 0x01){	//check the status bit to see if the counter reached 0 
 		
 		// Toggle GPIO pin state  and 
-		pinState = !pinState; 				// to change current HIGH and LOW 
-        	GPIO_Set(ENABLE_PIN, pinState);
+		pinState = !pinState; 				
+		// to change current HIGH and LOW 
+        GPIO_Set(ENABLE_PIN, pinState);
 
-		// scale adc for duty cycle 
-		uint32_t loadOn = LOAD  *  adcValue / 4095;	 // double check this when implementing 
+		// scale adc for duty cycle for pwm 
+		uint32_t loadOn = LOAD  *  adcValue / 4095;
 		uint32_t loadOff = LOAD - loadOn;
-		printf("Load On: %d, Load Off: %d\n", loadOn, loadOff);
+		printf("Load On: %d, Load Off: %d\n", loadOn, loadOff); // toruble shooting 
 		
 
 		// Load the next value to the timer duration
 		if (pinState) {
 			timer->load = loadOn;
-			//InitTimer(loadOn);
-			printf("high\n");
 		}else {
 			timer->load = loadOff;
-			//InitTimer(loadOff);
-			printf("low\n");
 		}
-
-		//timer->control = 0b01;	
-		
 	}
 }
 
@@ -179,7 +182,7 @@ void SigControl(){
 int main(void) {		
 	// initialize the GPIO and timer
 	GPIO_Init();
-	InitTimer(LOAD); 		// start at the off state 
+	InitTimer(LOAD); 
 
     volatile int delay_count;//to track delaying for loop
     //polling much faster than internal timers, so using main board to calculate delay
@@ -188,7 +191,9 @@ int main(void) {
 
 	// running loop 
 	while(1){
+		// for incrementing hex display 
 		ButtonControl();
+		// main function for producing pwm from reading the poteniometer 
 		SigControl();
 	}
 	
